c184a93d131af00bc87c0513b0d251db0b3162c6
Deep <45520903+deepmeena70@users.noreply.github.com>
2023-01-27 15:32:11 +0530

    implemented library

--- main.js
+++ main.js
@@ -1,70 +1,24 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const toRadians = (degrees) => (degrees * Math.PI) / 180;
-let particles = [];
+import { particlesCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
 
-// particle tracing circle
-class Particle {
-  constructor(x, y, deg) {
-    this.x = x;
-    this.y = y;
-    this.size = 6;
-    this.color = 'red';
-    this.deg = deg;
-    this.degIncrement = 1;
-  }
-  update() {
-    if (this.deg >= 360) {
-      this.deg = 0;
-    } else {
-      this.deg += this.degIncrement;
-    }
-  }
-  draw() {
-    ctx.globalAlpha = 0.1;
-    ctx.fillStyle = '#000000'
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.save();
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate((this.deg * Math.PI) / 180);
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.globalAlpha = 1;
-    ctx.fillStyle = this.color;
-    ctx.fill();
-    ctx.closePath();
-    ctx.restore();
-  }
-}
+const pc = particlesCursor({
+  el: document.getElementById('app'),
+  gpgpuSize: 512,
+  colors: [0x00ff00, 0x0000ff],
+  color: 0xff0000,
+  coordScale: 0.5,
+  noiseIntensity: 0.001,
+  noiseTimeCoef: 0.0001,
+  pointSize: 5,
+  pointDecay: 0.0025,
+  sleepRadiusX: 250,
+  sleepRadiusY: 250,
+  sleepTimeCoefX: 0.001,
+  sleepTimeCoefY: 0.002
+})
 
-const fillParticles = (numberOfParticles) => {
-  for (let i = 0; i < numberOfParticles; i++) {
-    particles.push(new Particle(140, 140, 0));
-  }
-};
-
-const setCanvas = () => {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-};
-
-const animate = () => {
-  particles.forEach((particle) => {
-    particle.update();
-    particle.draw();
-  });
-  requestAnimationFrame(animate);
-};
-
-addEventListener('load', () => {
-  setCanvas();
-  fillParticles(1);
-  animate();
-});
-
-addEventListener('resize', () => {
-  particles = [];
-  setCanvas();
-  fillParticles(1);
-  animate();
-});
+document.body.addEventListener('click', () => {
+  pc.uniforms.uColor.value.set(Math.random() * 0xffffff)
+  pc.uniforms.uCoordScale.value = 0.001 + Math.random() * 2
+  pc.uniforms.uNoiseIntensity.value = 0.0001 + Math.random() * 0.001
+  pc.uniforms.uPointSize.value = 1 + Math.random() * 10
+})
